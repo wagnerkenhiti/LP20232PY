0a39d25da7d1ebc26f749be373467aaf36733eae
biancanakazawa <bimitiena@gmail.com>
2024-04-02 01:23:21 -0400

    Menus principais

--- apresentacao.py
+++ apresentacao.py
@@ -3,7 +3,7 @@ from os import system, name
 #################################################################
 def limpaTela():
     '''
-    Limpa a tela de acordo com o systema operacional
+    Limpa a tela de acordo com o sistema operacional
     '''
     if name == 'nt':
         _ = system("cls")
@@ -12,26 +12,107 @@ def limpaTela():
 
 #################################################################        
 
-def MenuPrincipal() -> str :
+def MenuPrincipal() -> int :
     '''
-    Exemplo de Menu principal para o sistema
-    
-    Retorno    
-    -------
-    Retorna válida escolhida
+    Menu principal
 
+    Retorno
+    -------
+    Retorna a opção escolhida pelo usuário
     '''
-    opcoes = [1,2,3,9]
+    opcoes= [1, 2, 3, 9]
     opcao = 10
     while opcao not in opcoes:
         limpaTela()
-        print("#"*20)
-        print("1.Locações\n2.Clientes\n3.Carros\n9.Sair")    
-        print('#'*20)
-        opcao = int(input("Opção -> "))            
+        print("#"*50)
+        print("1. Locação")
+        print("2. Cliente")
+        print("3. Carro")
+        print("9. Sair")
+        print("-"*50)
+        opcao = int(input("Opcao ->"))
+        print("-"*50)
+        print("#"*50)
     return opcao
 
-#################################################################    
+#################################################################
+
+def MenuLocacao() -> int:
+    '''
+    Menu para locações de carros
+
+    Retorno
+    -------
+    Retorna opção escolhida pelo usuário
+    '''
+    opcoes = [1, 2, 3, 9]
+    opcao = 10
+    while opcao not in opcoes:
+        limpaTela()
+        print("#"*50)
+        print("1. Cadastrar uma nova locação")
+        print("2. Encerrar uma locação")
+        print("3. Carros disponíveis para locação")
+        print("4. Relatório de carros locados")
+        print("9. Sair")
+        print("-"*50)
+        opcao = int(input("Opcao -> "))
+        print("-"*50)
+        print("#"*50)
+    return opcao
+
+#################################################################
+
+def MenuCliente() -> int:
+    '''
+    Menu para cliente
+
+    Retorno
+    -------
+    Retorna a opção escolhida pelo usuário
+    '''
+    opcoes = [1, 2, 3, 4, 9]
+    opcao = 10
+    while opcao not in opcoes:
+        limpaTela()
+        print("#"*50)
+        print("1. Cadastar novo cliente")
+        print("2. Atualizar dados de cliente")
+        print("3. Excluir dados de cliente")
+        print("4. Localizar locações de um cliente")
+        print("9. Sair")
+        print("-"*50)
+        opcao = int(input("Opcao -> "))
+        print("-"*50)
+        print("#"*50)
+    return opcao
+#################################################################
+
+def MenuCarro() -> int:
+    '''
+    Menu para carro
+
+    Retorno
+    -------
+    Retorna a opção escolhida pelo usuário
+    '''
+    opcoes = [1, 2, 3, 4, 9]
+    opcao = 10
+    while opcao not in opcoes:
+        limpaTela()
+        print("#"*50)
+        print("1. Cadastrar novo carro")
+        print("2. Atualizar dados de um carro")
+        print("3. Excluir dados de um carro")
+        print("4. Carros de carros a venda")
+        print("9. Sair")
+        print("-"*50)
+        opcao = print(input("Opcao -> "))
+        print("-"*50)
+        print("#"*50)
+    return opcao
+
+#################################################################  
 
 def CadastrarCliente() -> dict :
     '''
