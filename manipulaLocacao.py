12e3cf61bd9247989c8231aefd248b58cbe50355
biancanakazawa <bimitiena@gmail.com>
2024-04-07 18:30:05 -0400

    item 8 (provavelmente completo) e item 5 (incompleto)

--- manipulaLocacao.py
+++ manipulaLocacao.py
@@ -1,5 +1,6 @@
 import apresentacao
 import csv
+import datetime
 
 def novaLocacao():
     '''
@@ -7,16 +8,72 @@ def novaLocacao():
     item(4)
     '''
 
-def encerraLocacao():
+def encerraLocacao() -> bool:
     '''
     Função que encerra a locação de um carro
-    item(5)
+
+    Retorno
+    -------
+    Retorna True se a locação foi encerrado corretamente
     '''
+    #Número de identificação da locação
+    identificacao = input("Identificação da locação: ")
+
+    #Procurar o valor da diária do carro no Carros.csv
+    try:
+        arq = open("Carros.csv")
+        listaCarros = csv.DictReader(arq, delimiter=";")
+        for carro in listaCarros:
+            if carro['Identificacao'] == identificacao:
+                valor_diaria = carro['Diaria']
+        arq.close()
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        return False
+
+    #Procurar data de início da locação em Locacoes.csv
+
+    #Data de encerramento da locação
+    saida = input("Data da devolução (dia/mes/ano): ")
+    horario_saida = input("Horário de devolução (hh:mm): ")
+    saida = saida + " " + horario_saida
+    data_saida = datetime.datetime.strptime(saida)
+
+    #Calculo da quantidade de tempo decorrido
+    tempo_decorrido = data_saida - data_entrada
+    print(tempo_decorrido)
+    if tempo_decorrido.days > 0:
+        [dummy, horas] =  str(tempo_decorrido).split(',')
+        [horas, minutos, segundos] = horas.split(":")    
+    else:
+        [horas, minutos, segundos] = str(tempo_decorrido).split(":")
+
+    #Exibindo horas e minutos utilizados, conferir se o calculo foi feito corretamente (tirar depois)
+    dias = tempo_decorrido.days
+    print(f"{dias} dias e {horas} horas utilizadas" )
+
+    #Calculo do valor da locação
+    valor_pagar = (dias * valor_diaria) + (horas/24 * valor_diaria)
+
+    #Quilometragem do carro no momento da entrega
+    quilometragem = float(input("Quilometragem do carro: "))
+
+    #Atualizar dados em Locacoes.csv
+    
+    #Atualizar dados em Carros.csv
 
 def carrosDisponiveis(categoria_procurar) -> bool:
     '''
-    Função que mostra quais carros estão disponiveis para locação
-    item(8)
+    Função que mostra quais carros de determinada categoria estão disponiveis para locação
+
+    Parâmetros
+    ----------
+    categoria_procurar: string com o nome da categoria
+
+    Retorno
+    -------
+    Retorna True se o arquivo Carros.csv foi aberto com sucesso
+    Retorna False se o arquivo Carros.csv não foi encontrado
     '''
     try:
         arq = open("Carros.csv")
