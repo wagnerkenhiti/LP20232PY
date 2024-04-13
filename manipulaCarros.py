90a166f6e591d77d874655c836c533332685dfc9
Fabio Ventura <fabioventura61@gmail.com>
2024-04-13 08:58:26 -0400

    implementacao funcoes item 2. carros

--- manipulaCarros.py
+++ manipulaCarros.py
@@ -38,20 +38,55 @@ def cadastrar(listaCarros : list) -> bool:
 
 #################################################################
 
-def alterar():
-    '''
-    Função para alterar dados de um carro
-    item(2)
-    '''
+def alterar( placa : str) -> bool:
+    linhas = []
+    try:
+        with open('carros.csv', 'r', newline='') as arquivo_origem:
+            linhas = list(csv.DictReader(arquivo_origem, delimiter=';'))
+
+        with open('carros.csv', 'w', newline='') as arquivo_destino:
+            nomes_colunas = ['Identificacao', 'Modelo', 'Cor', 'AnoFabricacao', 'Placa', 'Cambio', 'Categoria', 'Km', 'Diaria', 'Seguro', 'Disponivel']
+            escritor = csv.DictWriter(arquivo_destino, fieldnames=nomes_colunas, delimiter=';')
+            escritor.writeheader()
+
+            for linha in linhas:
+                if linha['Placa'] == placa:
+                    campo_alterar = input('Qual campo do carro você deseja alterar? (Identificacao;Modelo;Cor;AnoFabricacao;Placa;Cambio;Categoria;Km;Diaria;Seguro;Disponivel): ')
+                    
+                    # Verificando se o campo inserido é válido
+                    while campo_alterar not in nomes_colunas:
+                        print('Campo inválido. Por favor, insira um campo válido.')
+                        campo_alterar = input('Qual campo do carro você deseja alterar? (Identificacao;Modelo;Cor;AnoFabricacao;Placa;Cambio;Categoria;Km;Diaria;Seguro;Disponivel): ')
+                    
+                    novo_valor = input(f'Informe o novo valor para {campo_alterar}: ')
+                    linha[campo_alterar] = novo_valor
+
+                escritor.writerow(linha)
+        
+        return True
+    except FileNotFoundError:
+        print('Arquivo não encontrado. Retornando False.')
+        return False
 
 #################################################################
 
-def excluir():
-    '''
-    Função para excluir dados de um carro
-    item(2)
-    '''
-
+def excluir(placa : str) -> bool:
+    linhas = []
+    try:
+        with open('Carro.csv' , 'r') as arquivo:
+            leitor = csv.DictReader(arquivo)
+            for linha in leitor:
+                if linha['Placa'] != placa:
+                    linhas.append(linha)
+        with open('Carro.csv', 'w', newline='') as arquivo:
+            nomes_colunas = ['Identificacao', 'Modelo', 'Cor', 'AnoFabricacao', 'Placa', 'Cambio', 'Categoria', 'Km', 'Diaria', 'Seguro', 'Disponivel']
+            escritor = csv.DictWriter(arquivo, fieldnames=nomes_colunas)
+            escritor.writeheader()
+            escritor.writerows(linhas)
+        return True
+    except FileNotFoundError:
+        print('Arquivo não encontrado.')
+        return False
 #################################################################
 
 def venda():
